39e5446172177402d9d64648fa006a6772cf9d81
Dipankar Das <dipankardas0115@gmail.com>
2023-04-15 19:27:26 +0530

    added the manifest files
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- http-server/main.go
+++ http-server/main.go
@@ -7,15 +7,18 @@ import (
 )
 
 // make them accessable from redis database
-type album struct {
+type Album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
+type Home struct {
+}
+
 // make them accessible from redis database
-var albums = []album{
+var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
@@ -25,8 +28,12 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getHome(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"home": "Welcome to restful http server backed by redis", "purpose": "napptive hackathon"})
+}
+
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newAlbum Album
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
@@ -50,6 +57,7 @@ func getAlbumByID(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	router.GET("/", getHome)
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 	router.GET("/albums/:id", getAlbumByID)
